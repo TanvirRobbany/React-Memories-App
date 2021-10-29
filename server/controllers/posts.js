0063e117a2ebd034d88defaf6c1c0b3483d9c141
TanvirRobbany <trobbany@cisscom.com>
2021-10-30 02:14:07 +0600

    Post Updated Routes,Controller Added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Posts from '../models/posts.js'
 
 export const getPosts = async (req, res) => {
@@ -21,4 +22,20 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({message: error.message});
     }
+}
+
+export const updatePost = async (req, res) => {
+    const id = req.params;
+    const post = req.body;
+
+    let postId = {
+        _id: id
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No posts found')
+    }
+
+    const updatedPost = await Posts.findByIdAndRemove(postId, post, {new: true});
+    res.status(201).json(updatedPost)
 }
