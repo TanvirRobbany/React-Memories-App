21aaea953c3ae9b481c644b8d12f497737f2da42
Tanvir Ahmed Robbany <tanvirahmed80151@gmail.com>
2021-10-31 18:00:19 +0600

    Modified Server to Handle Bigger Files < 30MB

--- server/server.js
+++ server/server.js
@@ -4,13 +4,18 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
-const app = express();
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 dotenv.config()
 
 app.use(express.json({extended: false}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to memories API!');
+})
 
 const PORT = process.env.PORT || 5000;
 
